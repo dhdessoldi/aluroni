0e0618043b7f94f20ce42c60d9e03c74f810b36b
dhdessoldi <dhdessoldi@hotmail.com>
2023-05-30 20:08:51 -0300

    Projeto finalizado

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable indent */
 import React, { useState, useEffect } from 'react';
 import cardapio from 'data/cardapio.json';
 import Item from './Item';
 import styles from './Itens.module.scss';
+import { Cardapio } from 'types/Prato';
 
 interface Props {
   busca: string,
@@ -24,12 +26,12 @@ export default function Itens(props: Props) {
     return true;
   }
 
-  function ordenar(novaLista: typeof cardapio) {
+  function ordenar(novaLista: Cardapio) {
     switch (ordenador) {
-    case 'porcao': return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
-    case 'qtd_pessoas': return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
-    case 'preco': return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
-    default: return novaLista;
+      case 'porcao': return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+      case 'qtd_pessoas': return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
+      case 'preco': return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
+      default: return novaLista;
     }
   }
 
